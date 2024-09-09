7db8c31c0c50702178667894f9e8512c7def09ba
aserafim <correia.alefe@gmail.com>
2024-09-09 13:59:09 -0300

    :sparkles: Entendedo Declaração e atribuição

---
+++ 01 - Aula 1/a.go
@@ -0,0 +1,10 @@
+//package soma
+
+//Observe o erro
+//todos os arquivos dentro de um mesmo
+//diretorio devem pertencer a um mesmo
+//pacote
+
+package main
+
+var a string = "Teste"
---
+++ 01 - Aula 1/main.go
@@ -0,0 +1,14 @@
+/*
+O nome do pacote, em geral é o nome do
+diretório aonde está o arquivo.
+Com exceção do package main, que é o
+principal, e dos testes.
+*/
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(a)
+}
