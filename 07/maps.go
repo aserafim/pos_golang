6458f9e70613899d06cae4936a0b77bc4df99294
aserafim <correia.alefe@gmail.com>
2024-09-10 13:50:42 -0300

    :sparkles: feat: Trabalhando com Maps

---
+++ 07/maps.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Maps")
+
+	//var meuMapa = map[int]int{}
+
+	salarios := map[string]float32{
+		"Alefe":   8000.34,
+		"Natalia": 11000,
+	}
+
+	fmt.Println(salarios["Natalia"])
+
+	//salarios2 := make(map[int]int)
+
+	//var array [21]int
+	for nome, salario := range salarios {
+		fmt.Printf("O salário de %s é %.2f.\n", nome, salario)
+	}
+}
