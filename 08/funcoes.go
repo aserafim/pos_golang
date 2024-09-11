388d67f166fbed11c5d21a0794f544096fb23fda
aserafim <correia.alefe@gmail.com>
2024-09-11 14:57:30 -0300

    :sparkles: :feat Exercícios sobre funções

--- 08/funcoes.go
+++ 08/funcoes.go
@@ -3,10 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
-//Exercício 1 - Função de Soma: Escreva uma função chamada sum que receba 
-//dois inteiros como parâmetros e retorne a soma deles.
+// Exercício 1 - Função de Soma: Escreva uma função chamada sum que receba
+// dois inteiros como parâmetros e retorne a soma deles.
 func sum(a int, b int) int {
 	return a + b
 }
@@ -16,13 +17,14 @@ func sum(a int, b int) int {
 // 	return a + b
 // }
 
-// Funções com múltiplos retornos
-// são úteis no tratamento de erros
-func div(a int, b int) (int, bool) {
+// Exercício 5 - Função com Múltiplos Retornos: Crie uma função chamada
+// divide que receba dois números inteiros e retorne o quociente e o resto
+// da divisão entre eles.
+func divide(a int, b int) (int, int) {
 	if b != 0 {
-		return a / b, true
+		return (a / b), (a % b)
 	}
-	return 0, false
+	return 0, 0
 }
 
 // Funções com múltiplos retornos
@@ -34,19 +36,74 @@ func divWithErros(a int, b int) (int, error) {
 	return 0, nil
 }
 
-//Exercício 2 -Função de Verificação de Paridade: Crie uma função chamada 
-//isEven que receba um número inteiro e retorne true se o número for par, 
-//e false caso contrário.
+// Exercício 2 -Função de Verificação de Paridade: Crie uma função chamada
+// isEven que receba um número inteiro e retorne true se o número for par,
+// e false caso contrário.
+func isEven(num int) bool {
+	if num%2 == 0 {
+		return true
+	}
+	return false
+}
 
+// Exercício 3 - Função de Fatorial: Implemente uma função chamada
+// factorial que receba um número inteiro positivo e retorne o fatorial
+// desse número.
+func factorial(num int) int {
+	result := 1
+	if num == 1 {
+		return 1
+	} else if num < 1 {
+		return 0
+	}
+	for i := num; i >= 1; i-- {
+		result *= i
+	}
+	return result
+}
 
+// Exercício 4 - Função para Reverter String: Escreva uma função chamada
+// reverseString que receba uma string como parâmetro e retorne a string
+// invertida.
+func reverseString(texto string) string {
+	reversedString := make([]string, len(texto))
+	println(len(texto))
+	j := len(texto) - 1
+
+	for i := 0; i < len(texto); i++ {
+		reversedString[i] = string(rune(texto[j]))
+		j--
+	}
+	return strings.Join(reversedString, "")
+}
+
+// Exercício 6 - Função Recursiva de Fibonacci: Implemente uma função
+// chamada fibonacci que receba um número inteiro n e retorne o enésimo
+// número da sequência de Fibonacci de forma recursiva.
+
+// Exercício 7 - Função para Ordenar uma Lista: Escreva uma função chamada
+// sortArray que receba um slice de inteiros e retorne esse slice ordenado.
+
+// Exercício 8 - Função para Encontrar o Maior Número: Crie uma função
+// chamada maxNumber que receba um slice de inteiros e retorne o maior
+// número presente no slice.
 
 func main() {
 
 	fmt.Println("Funções")
 
-	println(sum(1, 2))
+	// println(sum(1, 2))
 
-	println(div(2, 0))
+	// println(div(2, 0))
 
-	println(divWithErros(2, 0))
+	// println(divWithErros(2, 0))
+
+	//println(isEven(0))
+
+	//println(factorial(1))
+
+	//println(reverseString("alefe"))
+
+	println(divide(7, 2))
+
 }
