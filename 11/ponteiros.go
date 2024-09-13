b9976e7cb8116ac0f9b3845be4e75cfa260dc683
aserafim <correia.alefe@gmail.com>
2024-09-13 13:25:39 -0300

    :sparkles: :feat Aulas sobre ponteiros

--- 11/ponteiros.go
+++ 11/ponteiros.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func addDez(a *int) {
+	*a += 10
+}
+
 func main() {
 
 	fmt.Println("Ponteiros")
@@ -9,11 +13,16 @@ func main() {
 	// Memória -> endereço -> Valor
 	a := 10
 	println(a)
+
+	// Sempre que virmos um & seguido
+	// de um nome de variável
+	// estamos nos referindo ao
+	// nome do endereço (código hash)
 	println(&a)
 
 	// Sempre que virmos um * seguido
 	// de um tipo estamos falando
-	// de um endereço de memória
+	// de um ponteiro para uma variável
 	var ponteiro *int = &a
 	println(ponteiro)
 
@@ -21,6 +30,9 @@ func main() {
 	// seguido de um nome de variável
 	// estamos falando do que está
 	// armazenado naquela variável
-	*ponteiro = 20
+	*ponteiro = 0
+	println(a)
+
+	addDez(&a)
 	println(a)
 }
