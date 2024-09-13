454d0c1d8f273301d3c87bcc82d2bb2861055d99
aserafim <correia.alefe@gmail.com>
2024-09-13 14:59:59 -0300

    :sparkles: :feat Exercício sobre ponteiros

--- exercicios/interfaces/int_poli.go
+++ exercicios/interfaces/int_poli.go
@@ -2,12 +2,60 @@
 Interfaces e Polimorfismo: Crie uma interface chamada Shape que tenha um método Area() float64. Depois, crie duas structs: Circle e Rectangle. Ambas devem implementar a interface Shape. O Circle terá um raio (radius) e o Rectangle terá largura (width) e altura (height). Escreva um código que calcule a área de diferentes formas usando a interface Shape.
 */
 
+/*
+Interface com Métodos e Ponteiros: Crie uma interface chamada Database com os métodos Connect() e Disconnect(). Implemente essa interface para diferentes tipos de bancos de dados, como SQLDatabase e NoSQLDatabase, usando structs. Faça com que a função Connect altere um valor dentro da struct através de um ponteiro, representando o status da conexão.
+*/
+
 package main
 
 import (
 	"fmt"
 	"math"
 )
+
+type Cat struct {
+	nome  string
+	idade int
+}
+
+func (c Cat) Speak() {
+	fmt.Println("Miau!!")
+}
+
+type Database interface {
+	Connect()
+	Disconnect()
+}
+
+type SQLDatabase struct {
+	Name      string
+	Conectado bool
+}
+
+func (banco *SQLDatabase) Connect() {
+	banco.Conectado = true
+
+}
+
+func (banco *SQLDatabase) Disconnect() {
+	banco.Conectado = false
+
+}
+
+type NoSQLDatabase struct {
+	Name      string
+	Conectado bool
+}
+
+func (banco *NoSQLDatabase) Connect() {
+	banco.Conectado = true
+
+}
+
+func (banco *NoSQLDatabase) Disconnect() {
+	banco.Conectado = false
+
+}
 
 type Shape interface {
 	Area() float64
@@ -46,4 +94,25 @@ func main() {
 	fmt.Printf("A área do círculo é %.2f\n", circuloUm.Area())
 	fmt.Printf("A área do retângulo é %.2f\n", retanguloUm.Area())
 
+	bancoUm := NoSQLDatabase{
+		Name:      "MongoDB",
+		Conectado: false,
+	}
+
+	fmt.Println(bancoUm)
+	bancoUm.Connect()
+	fmt.Println(bancoUm)
+	bancoUm.Disconnect()
+	fmt.Println(bancoUm)
+
+	bancoDois := SQLDatabase{
+		Name:      "MySQL",
+		Conectado: false,
+	}
+
+	fmt.Println(bancoDois)
+	bancoDois.Connect()
+	fmt.Println(bancoDois)
+	bancoDois.Disconnect()
+	fmt.Println(bancoDois)
 }
