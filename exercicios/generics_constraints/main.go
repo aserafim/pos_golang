34001f17cc01ecdd4eec30a07ea0e157e9c51f31
aserafim <correia.alefe@gmail.com>
2024-09-17 15:00:08 -0300

    :sparkles: :feat Alguns exercícios sobre Generics e Constraints

--- exercicios/generics_constraints/main.go
+++ exercicios/generics_constraints/main.go
@@ -1,9 +1,3 @@
-package main
-
-import (
-	"fmt"
-)
-
 /*
 1. Função Genérica para Encontrar o Maior Elemento
 Crie uma função genérica que recebe um slice de qualquer
@@ -13,17 +7,57 @@ genérico seja um número (inteiro ou float).
 */
 
 /*
-3. Função Genérica de Comparação
-Escreva uma função genérica que aceita dois valores de um tipo genérico e retorna o maior entre eles. Use constraints para permitir apenas tipos que suportem a comparação.
-
-4. Função Genérica de Soma
-Crie uma função genérica que recebe um slice de números (inteiros ou floats) e retorna a soma de todos os elementos.
+2. Implementação de uma Fila Genérica
+Implemente uma fila (queue) usando uma
+estrutura de dados genérica.
+A fila deve ter as funções de enfileirar (enqueue),
+desenfileirar (dequeue) e retornar o tamanho (size).
+Teste a fila com diferentes tipos,
+como int, string e float64.
 */
 
+/*
+3. Função Genérica de Comparação
+Escreva uma função genérica que aceita
+dois valores de um tipo genérico e retorna
+o maior entre eles. Use constraints para
+permitir apenas tipos que suportem a comparação.
+*/
+
+/*
+4. Função Genérica de Soma
+Crie uma função genérica que recebe
+um slice de números (inteiros ou floats)
+e retorna a soma de todos os elementos.
+*/
+
+/*
+5. Mapa Genérico
+Implemente uma função genérica
+que recebe um mapa de chaves e valores
+de tipos genéricos e retorna uma lista
+de todas as chaves.
+*/
+
+/*
+6. Ordenação de Slice Genérico
+Crie uma função genérica para ordenar
+slices de qualquer tipo que suporte
+operações de comparação, como int,
+float64 ou string. Utilize constraints
+adequadas para restringir os tipos permitidos.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+// Exercício 1
 type Number interface {
 	~int | ~float64 | ~float32
 }
-
 
 func partition[T Number](arr []T, low, high int) ([]T, int) {
 	pivot := arr[high]
@@ -48,10 +82,24 @@ func quickSort[T Number](arr []T, low, high int) ([]T, T) {
 	return arr, arr[len(arr)-1]
 }
 
+// Exercício 3
+func RetornaMaior[T Number](num1 T, num2 T) T {
+	if num1 > num2 {
+		return num1
+	}
+	return num2
+}
+
 func main() {
 	fmt.Println("Exercícios")
 
 	array := []int{50, 40, 30, 20, 10}
+	arrayDois := []int{5.0, 4.0, 3.0, 2.0, 1.0}
 	_, maior := quickSort(array, 0, len(array)-1)
 	fmt.Println(maior)
+
+	_, maiorDois := quickSort(arrayDois, 0, len(arrayDois)-1)
+	fmt.Println(maiorDois)
+
+	fmt.Println(RetornaMaior(1.0, 8))
 }
