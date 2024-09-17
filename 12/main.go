9b1681a074d2f58042e2fce7fb50f0f31cac21c0
aserafim <correia.alefe@gmail.com>
2024-09-17 14:07:38 -0300

    :sparkles: :feat Comparando Constraints

--- 12/main.go
+++ 12/main.go
@@ -25,7 +25,7 @@ func SomaValores[T int | float64](m map[string]T) T {
 // Podemos ainda trabalhar com
 // constraints
 type Number interface {
-	int | float32 | float64
+	~int | float32 | float64
 }
 
 func SomaValoresConstraints[T Number](m map[string]T) T {
@@ -34,6 +34,29 @@ func SomaValoresConstraints[T Number](m map[string]T) T {
 		soma += v
 	}
 	return soma
+}
+
+// Nem todo int é um int
+// MyNumber é um int
+// Porém não será aceito como
+// um Number (constraint)
+// a menos que coloquemos
+// um ~ no como prefixo
+// em nossa constraint
+type MyNumber int
+
+// Para compararmos Constraints
+// podemos usar uma constraint
+// específica, comparable
+// desse modo o Go permite
+// utilizar quaisquer valores
+// como parâmetros desde que
+// eles sejam valores comparáveis
+func Compara[T comparable](a T, b T) bool {
+	if a == b {
+		return true
+	}
+	return false
 }
 
 func main() {
@@ -46,5 +69,16 @@ func main() {
 
 	m3 := map[string]float32{"Alefe": 500.9, "Natalia": 1000.8, "Antonieta": 4000.7}
 	fmt.Printf("O valor final é %.2f\n", SomaValoresConstraints(m3))
+
+	//Esse trecho funciona porque foi colocado um ~ na definição da constraint
+	m4 := map[string]MyNumber{"Alefe": 500, "Natalia": 1000, "Antonieta": 4000}
+	fmt.Printf("O valor final é %d\n", SomaValoresConstraints(m4))
+
+	//Funcionamento da função com comparable
+	fmt.Println(Compara("a", "b"))
+	fmt.Println(Compara(1, 1))
+	fmt.Println(Compara(1.3, 1.5))
+	fmt.Println(Compara(1, 1.0))
+	//fmt.Println(Compara(1,"1.0")) Tipos não comparáveis -- não compila!
 
 }
