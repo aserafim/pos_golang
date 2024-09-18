6576057773ad43e75fb97cd6523902d836440616
aserafim <correia.alefe@gmail.com>
2024-09-18 12:15:09 -0300

    :sparkles: :feat Aula sobre Loops

--- 13/main.go
+++ 13/main.go
@@ -14,4 +14,8 @@ func main() {
 	//fmt.Println(math.a) Não compila, pois "variaveis minúsculas" só são vistas dentro do pacote
 
 	fmt.Println(math.A) //Variáveis maiúsculas podem ser usadas fora do pacote
+
+	// O mesmo vale para structs com inicial maiúscula ou minúscula
+	// e o mesmo para os campos da struct que estão iniciados
+	// com letra maiúscula ou minúscula
 }
