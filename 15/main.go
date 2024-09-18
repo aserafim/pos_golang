4e6d349c2e994c8497c2533efe7b3a510543ca24
aserafim <correia.alefe@gmail.com>
2024-09-18 12:26:13 -0300

    :sparkles: :feat Aula sobre Condicionais

--- 15/main.go
+++ 15/main.go
@@ -5,4 +5,29 @@ import "fmt"
 func main() {
 
 	fmt.Println("Condicionais")
+
+	a := 0
+	b := 1
+	c := 2
+
+	if a > b {
+		fmt.Println("Maior")
+	}
+
+	if a > b && b > c {
+		fmt.Println("Maior")
+	}
+
+	if a > b || b > c {
+		fmt.Println("Maior")
+	}
+
+	switch a {
+	case 0:
+		fmt.Println(a)
+	case 2:
+		fmt.Println(b)
+	default:
+		fmt.Println("default")
+	}
 }
