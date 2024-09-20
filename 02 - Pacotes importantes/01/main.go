b4129e2195cd8b5549e068ca840ab73e91b3ff27
aserafim <correia.alefe@gmail.com>
2024-09-20 14:21:34 -0300

    :sparkles: :feat Aula sobre arquivos

---
+++ 02 - Pacotes importantes/01/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Manipulando arquivos")
+
+	// Criação/Escrita de arquivo
+	f, err := os.Create("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	tamanho, err := f.Write([]byte("Escrevendo dados no arquivo criado."))
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Tamanho do arquivo: %d bytes.\n", tamanho)
+	f.Close()
+
+	// Leitura
+	arquivo, err := os.ReadFile("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	// o casting para string
+	// é necessário pois os arquivos
+	// armazenam bytes e não chars/strings
+	fmt.Println(string(arquivo))
+
+	// Como ler arquivos muito grandes??
+	// por exemplo, um arquivo que tenha
+	// um tamanho maior do que a memória
+	// disponível?
+
+	// leitura parcial e contínua
+	file, err := os.Open("arquivo.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(file)
+	buffer := make([]byte, 10)
+
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			break
+		}
+		fmt.Println(string(buffer[:n]))
+	}
+
+}
