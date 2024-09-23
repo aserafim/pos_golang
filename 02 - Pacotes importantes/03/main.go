9a2bba4b2a4147c39ae551b207ac4de320db8610
aserafim <correia.alefe@gmail.com>
2024-09-23 09:59:25 -0300

    :sparkles: :feat Manipulando JSON

--- 02 - Pacotes importantes/03/main.go
+++ 02 - Pacotes importantes/03/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 type Conta struct {
@@ -22,4 +23,30 @@ func main() {
 	}
 
 	println(string(res))
+
+	// Podemos ainda usar um encoder
+	// que vai transformar em um JSON
+	// e salvar num arquivo de saída
+	// isso é útil quando queremos
+	// realizar automaticamente a
+	// "entrega" para algum servidor
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.Encode(conta)
+
+	// Agora vamos fazer o trabalho oposto
+	// a desserialização
+	// Note que aqui precisamos de uma
+	// struct onde o json será armazenado
+	// após o resultado da desserialização
+	// ser finalizado
+	jsonPuro := []byte(`{"Numero":3, "Saldo":8115}`)
+	var contaX Conta
+	err = json.Unmarshal(jsonPuro, &contaX)
+	if err != nil {
+		panic(err)
+	}
+
+	println(contaX.Numero)
+	println(contaX.Saldo)
+
 }
