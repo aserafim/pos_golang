9958e84531b3ba8f715b2e849072820b904e7e94
aserafim <correia.alefe@gmail.com>
2024-09-23 09:51:51 -0300

    :sparkles: :feat Manipulando JSON

--- 02 - Pacotes importantes/03/main.go
+++ 02 - Pacotes importantes/03/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
+
+type Conta struct {
+	Numero int
+	Saldo  int
+}
 
 func main() {
 
 	fmt.Println("Aula sobre json")
+
+	conta := Conta{Numero: 1, Saldo: 4000}
+
+	res, err := json.Marshal(conta)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(res))
 }
