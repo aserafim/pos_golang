70d1bcaddf3e18a2c5172d474baeed03e9036e7d
aserafim <correia.alefe@gmail.com>
2024-09-24 10:12:20 -0300

    :sparkles: :feat Introduzindo pacote Http

--- 00 - Projeto 1.1/main.go
+++ 00 - Projeto 1.1/main.go
@@ -3,5 +3,10 @@ package main
 import "net/http"
 
 func main() {
+	http.HandleFunc("/", BuscaCep)
 	http.ListenAndServe(":8080", nil)
+}
+
+func BuscaCep(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!\n"))
 }
