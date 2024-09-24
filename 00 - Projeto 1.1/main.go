cac79613312c4d2a4592f36df24ad28ea64e785f
aserafim <correia.alefe@gmail.com>
2024-09-24 10:19:53 -0300

    :sparkles: :feat Trabalhando com pacote Http no Projeto

--- 00 - Projeto 1.1/main.go
+++ 00 - Projeto 1.1/main.go
@@ -3,10 +3,22 @@ package main
 import "net/http"
 
 func main() {
-	http.HandleFunc("/", BuscaCep)
+	http.HandleFunc("/", BuscaCepHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func BuscaCep(w http.ResponseWriter, r *http.Request) {
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Not Found!\n"))
+		return
+	}
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello, World!\n"))
 }
