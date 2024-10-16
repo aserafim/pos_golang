197b0abc72d9024432bbf58f112a37cf6572072c
aserafim <correia.alefe@gmail.com>
2024-10-16 08:15:25 -0300

    Inserindo dados no DB

--- 04 - Banco de Dados/1/main.go
+++ 04 - Banco de Dados/1/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
@@ -16,6 +21,45 @@ func NewProduct(name string, price float64) *Product {
 	}
 }
 
+func insertProduct(db *sql.DB, product *Product) error {
+	// Protege contra sql injection
+	stmt, err := db.Prepare("insert into products(id, name, price) values(?,?,?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	// O _ abaixo, se utilizado como res, por exemplo
+	// pode trazer algumas infos sobre a execução
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	prod := NewProduct("PineApple Phone", 3500.0)
+	err = insertProduct(db, prod)
+	if err != nil {
+		panic(err)
+	}
 
 }
