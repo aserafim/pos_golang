a83a1fddfb08309168f64c5daeccb36130eec138
aserafim <correia.alefe@gmail.com>
2024-11-13 19:14:28 -0300

    :sparkles: feat: Executando benchmarks

--- 08 - Testing/1/tax_test.go
+++ 08 - Testing/1/tax_test.go
@@ -41,3 +41,18 @@ func TestCalculateTaxBatch(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkCalculateTax(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax(500.0)
+	}
+}
+
+func BenchmarkCalculateTax2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		CalculateTax2(500.0)
+	}
+}
+
+
+//go test -bench=. 
