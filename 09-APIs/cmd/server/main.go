60fe90ee0fdce9ccdb7bad454379d1e9296cbc08
aserafim <correia.alefe@gmail.com>
2024-11-27 13:13:08 -0300

    Criada entidade User e função ValidatePassword

--- 09-APIs/cmd/server/main.go
+++ 09-APIs/cmd/server/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/aserafim/pos_golang/09-APIS/configs"
+)
+
 func main() {
-	//...
+	config, err := configs.LoadConfig(".")
+	println(config.DBDriver)
+
+	if err != nil {
+		println(err)
+	}
 }
