a601a2ab5671d5b6667fc4ca8c79360876f3dadb
aserafim <correia.alefe@gmail.com>
2024-12-04 19:17:01 -0300

    Teste createUser passed

--- 09-APIs/internal/infra/database/user_db.go
+++ 09-APIs/internal/infra/database/user_db.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/aserafim/pos_golang/09-APIS/internal/entity"
 	"gorm.io/gorm"
 )
 
@@ -10,4 +11,19 @@ type User struct {
 
 func NewUser(db *gorm.DB) *User {
 	return &User{DB: db}
+}
+
+// Se conseguir criar o usuário
+// o "atributo" DB vai ser populado
+// do contrário retornará um erro
+func (u *User) Create(user *entity.User) error {
+	return u.DB.Create(user).Error
+}
+
+func (u *User) FindByEmail(email string) (*entity.User, error) {
+	var user entity.User
+	if err := u.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
--- 09-APIs/pkg/entity/tolerance.go
+++
@@ -1,7 +0,0 @@
-/*
-O Go considera dois floats diferentes
-mesmo que a diferença esteja nas últimas
-casas decimais.
-Esta função ajuda a contonar esses problemas
-*/
-package entity
