09754d914efab179602f07c97fb69adbe97fd8ac
aserafim <correia.alefe@gmail.com>
2024-12-05 20:50:39 -0300

    Criando testes para ProductDB

---
+++ 09-APIs/internal/infra/database/product_db_test.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/aserafim/pos_golang/09-APIS/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+//ID        entity.ID `json:"id"`
+//Name      string    `json:"name"`
+//Price     float32   `json:"price"`
+//CreatedAt time.Time `json:"created_at"`
+
+func TestCreateProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.Product{})
+	product, _ := entity.NewProduct("IPhono 12", 12999.00)
+	productDB := NewProduct(db)
+
+	err = productDB.Create(product)
+	assert.Nil(t, err)
+
+	var productFound entity.Product
+	err = db.First(&productFound, "id = ?", product.ID).Error
+	assert.Nil(t, err)
+	assert.Equal(t, product.ID, productFound.ID)
+	assert.Equal(t, product.Name, productFound.Name)
+	assert.Equal(t, product.CreatedAt, productFound.CreatedAt)
+	assert.Equal(t, product.Price, productFound.Price)
+}
