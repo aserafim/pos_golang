9fba85d8a44bb4d5a68de626700ccb72062b468d
aserafim <correia.alefe@gmail.com>
2025-02-21 11:48:03 -0300

    Simulando problemas reais v1

--- 10 - Multithreading/03 - Concorrência no mundo real/main.go
+++ 10 - Multithreading/03 - Concorrência no mundo real/main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
 )
 
+var number uint64 = 0
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	number := 1
-	w.Write([]byte(fmt.Sprintf("You are the visitant of number %s", string(number))))
+	if r.URL.Path == "/favicon.ico" {
+		http.NotFound(w, r)
+		return
+	}
+
+	m := sync.Mutex{}
+	m.Lock()
+	number = number + 1
+	m.Unlock()
+	time.Sleep(600 * time.Microsecond)
+	w.Write([]byte(fmt.Sprintf("You are the visitant of number %d", number)))
+
 }
 
 func main() {
