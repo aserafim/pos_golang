5357f10422f268f98b21f7697acd8d53731257ba
aserafim <correia.alefe@gmail.com>
2025-03-06 20:13:04 -0300

    Range with Wait Group

---
+++ 10 - Multithreading/04 - Channels/03 - Range-With-Wait-Group/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	channel := make(chan int)
+	wait := sync.WaitGroup{}
+	wait.Add(10)
+
+	go publish(channel)
+	reader(channel, &wait)
+
+	wait.Wait()
+
+}
+
+func publish(ch chan int) {
+	for i := 1; i < 11; i++ {
+		ch <- i
+	}
+	// Evita o deadlock
+	close(ch)
+}
+
+func reader(ch chan int, w *sync.WaitGroup) {
+	for x := range ch {
+		fmt.Printf("Recebido %d\n", x)
+		w.Done()
+	}
+}
