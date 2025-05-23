960a4dcde90aa2469323b540e9c906502638f5ed
Alefe Serafim <correia.alefe@gmail.com>
2025-05-23 08:11:43 -0300

    Criada User entity e id package

---
+++ 09 - APIs/internal/entity/user.go
@@ -0,0 +1,34 @@
+package entity
+
+import (
+	"github.com/aserafim/pos_golang/09_APIs/pkg/entity"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       entity.ID `json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"-"`
+}
+
+// Funcao para criar um novo usuario
+func NewUser(name string, email string, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:       entity.NewID(),
+		Name:     name,
+		Email:    email,
+		Password: string(hash),
+	}, nil
+}
+
+// Metodo para validar se a senha do usuario confere com
+// o hash armazenado. Util para autenticacao
+func (u *User) ValidatePassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(password), []byte(u.Password))
+	return err == nil
+}
