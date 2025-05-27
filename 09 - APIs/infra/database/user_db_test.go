51a053f1a3aa9ebc8173a02abe9dec72821af005
Alefe Serafim <correia.alefe@gmail.com>
2025-05-27 15:04:43 -0300

    test: userDB

--- 09 - APIs/infra/database/user_db_test.go
+++ 09 - APIs/infra/database/user_db_test.go
@@ -17,7 +17,6 @@ func TestCreateUser(t *testing.T) {
 	db.AutoMigrate(&User{})
 	user, _ := entity.NewUser("Alefe", "a@a.com", "12345")
 	userDb := NewUser(db)
-
 	err = userDb.Create(user)
 
 	assert.Nil(t, err)
@@ -29,4 +28,24 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.Email, userFound.Email)
 	assert.Equal(t, user.Name, userFound.Name)
 	assert.NotNil(t, userFound.Password)
+}
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&User{})
+	user, _ := entity.NewUser("Alefe", "a@a.com", "12345")
+	userDb := NewUser(db)
+	err = userDb.Create(user)
+
+	assert.Nil(t, err)
+	userFound, err := userDb.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.NotNil(t, userFound.Password)
+
 }
