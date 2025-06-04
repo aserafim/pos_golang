a4dd1a045d2db6a3584f92796bdb746bcb4d8581
Alefe Serafim <correia.alefe@gmail.com>
2025-06-04 08:02:01 -0300

    refactor: Reorganizando handlers

--- 09 - APIs/cmd/server/main.go
+++ 09 - APIs/cmd/server/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/aserafim/pos_golang/09_APIs/configs"
 	"github.com/aserafim/pos_golang/09_APIs/internal/database"
-	"github.com/aserafim/pos_golang/09_APIs/internal/dto"
 	"github.com/aserafim/pos_golang/09_APIs/internal/entity"
+	"github.com/aserafim/pos_golang/09_APIs/internal/webserver/handlers"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -32,43 +31,10 @@ func main() {
 	productDB := database.NewProduct(db)
 
 	// Cria um productHandler
-	productHandler := NewProductHandler(productDB)
+	productHandler := handlers.NewProductHandler(productDB)
 
 	// Executa o servidor
 	http.HandleFunc("/products", productHandler.CreateProduct)
 	http.ListenAndServe(":8080", nil)
 
-}
-
-// Cria uma struct de Handler
-type ProductHandler struct {
-	ProductDB database.ProductInterface
-}
-
-// Cria um construtor para ProductHandler
-func NewProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-// Cria uma Handle Func para criar um novo Produto
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var productInput dto.CreateProductInput
-	err := json.NewDecoder(r.Body).Decode(&productInput)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	product, err := entity.NewProduct(productInput.Name, productInput.Price)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	err = h.ProductDB.Create(product)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
 }
---
+++ 09 - APIs/internal/webserver/handlers/product_handler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/aserafim/pos_golang/09_APIs/internal/database"
+	"github.com/aserafim/pos_golang/09_APIs/internal/dto"
+	"github.com/aserafim/pos_golang/09_APIs/internal/entity"
+)
+
+// Cria uma struct de Handler
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+// Cria um construtor para ProductHandler
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+// Cria uma Handle Func para criar um novo Produto
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var productInput dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&productInput)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product, err := entity.NewProduct(productInput.Name, productInput.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.Create(product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
